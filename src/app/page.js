e765cd1ac119e9c254de0bba762a446242f4c67e
Ramon Garza <ramongarza@Ramons-MacBook-Pro.local>
2023-06-22 16:51:38 -0500

    changed background color in layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
 // import Image from 'next/image'
 
-export default function Home() {
-  return (
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { fetchData } from "@/utils/api";
+
+export default function Weather() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    async function fetchDataFromApi() {
+      const weatherData = await fetchData();
+      console.log(weatherData);
+      setWeather(weatherData);
+    }
+    fetchDataFromApi();
+  }, []);
+
+  if(!weather) {
+    return <p className="text-3xl text-slate-300">Loading...</p>
+  }
+
+   return (
     <>
-      <h1>HELLO</h1>
+      <p className="text-slate-300">{JSON.stringify(weather)}</p>
     </>
   )
 }
