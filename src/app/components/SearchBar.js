0d36e8da188fa4e0d958b6dcc73b804f8d8d2c68
Ramon Garza <ramong5406@gmail.com>
2023-06-26 10:24:22 -0500

    disabled city search, added construction text

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -18,7 +18,7 @@ export default function SearchBar({ onSearch }) {
                     placeholder="Search City" 
                     className="text-slate-800 px-4 py-2 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-slate-500 focus:border-transparent" 
                 />
-                <button type="submit" className="bg-slate-500 hover:bg-slate-600 text-white px-4 py-2 rounded-r-lg transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-opacity-50">
+                <button disabled type="submit" className="bg-slate-500 hover:bg-slate-600 text-white px-4 py-2 rounded-r-lg transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-opacity-50">
                     Search
                 </button>
             </form>
